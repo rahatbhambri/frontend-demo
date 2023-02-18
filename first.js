25b07a152aa6d158b5eed94ba5b7827c63763ed1
TwistedPair <35606899+rahatbhambri@users.noreply.github.com>
2023-02-18 23:07:40 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,17 @@
+<html> 
+    <head>
+        <title>
+         Welcome Welcome       
+        </title>
+    </head> 
+
+    <body>
+        <script> 
+            var1 = 3
+            var2=  5
+            var func = new Function("a", "b",  "return a+b") 
+            var x = func(var1, var2) 
+            
+        </script>
+    </body>
+</html>
