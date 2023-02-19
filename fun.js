a4a662ac2e38b1182ceb113353c1a5f4092a176b
TezCheetah <TezCheetah@Users-MacBook-Pro.local>
2023-02-19 17:13:54 +0530

    changes

--- fun.js
+++ fun.js
@@ -1,13 +1,23 @@
 var button = document.querySelector('button') ;
 var box = document.getElementById('changeMe') ;
-var sum = document.getElementById('demo') ;
+var sum = document.getElementById('demo').innerHTML ;
 
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 button.onclick = function changeColour(){
-    let colours = ['red', "green", "blue", "pink", "purple"]
-    let i = getRandomInt(0, 4);
-    box.style.background = colours[i] ;
+    let colours = ['red', "green", "blue", "pink", "purple", "yellow"]
+    let i = getRandomInt(0, 5)
+    box.style.background = colours[i] 
+
+    let cons = parseInt(sum) 
+    
+    let newval = cons*2
+    sum = newval
+    document.getElementById('demo').innerHTML = sum
+
+    
+    
+    
 }
